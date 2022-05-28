c6362dab60b4f77e46835bbeec8e2510e188d67b
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-05-28 13:10:27 +0200

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=t8pPdKYpowI&t=4548s
+# 2:18:00
+
+my_list = ["January", "February", "March"]
+print(my_list)
+
+my_list[0] = "April"
+my_list[1] = "May"
+my_list[2] = "June"
+print(my_list)
