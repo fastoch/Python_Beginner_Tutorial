49de8164523eefed31716b93b2c44aa465315828
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-05-28 14:46:44 +0200

    Add files via upload

--- Lists.py
+++ Lists.py
@@ -1,12 +1,19 @@
 # https://www.youtube.com/watch?v=t8pPdKYpowI&t=4548s
-# 2:18:00
+# 2:20:00
 
+# creating a list
 my_list = ["January", "February", "March"]
 print(f"This is my list: {my_list} \n")
 
+# replacing items in a list
 my_list[0] = "April"
 my_list[1] = "May"
 my_list[2] = "June"
 print(f"This is my new list: {my_list} \n")
 
+# printing specific item
 print(f"This is the first item on my list: {my_list[0]} \n")
+
+# appending a new item to the list
+my_list.append("July")
+print(f"This is my appended list: {my_list} \n")
