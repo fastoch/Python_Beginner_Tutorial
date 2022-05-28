2cee52ede16ced6fba3eb2c07c73837483276a3c
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-05-28 13:17:59 +0200

    Add files via upload

--- Lists.py
+++ Lists.py
@@ -2,9 +2,11 @@
 # 2:18:00
 
 my_list = ["January", "February", "March"]
-print(my_list)
+print(f"This is my list: {my_list} \n")
 
 my_list[0] = "April"
 my_list[1] = "May"
 my_list[2] = "June"
-print(my_list)
+print(f"This is my new list: {my_list} \n")
+
+print(f"This is the first item on my list: {my_list[0]} \n")
