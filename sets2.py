536ec0606cdb936e460028a2c6b67842964a9d7f
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-03 21:12:27 +0200

    Add files via upload

--- sets2.py
+++ sets2.py
@@ -4,16 +4,24 @@
 """ 
 Basic set operations:
 - create a set
-- access items only via loop
+- access items only via a loop
 - add an item to the set
 - remove an item from the set
 """
 
 """
 Set are not lists:
+- sets do NOT allow DUPLICATE values
 - items in a set do not have a defined order
-- items cannot be referred to by index
+- items cannot be referred to by index, we can only access them via a loop
 - items cannot be changed, only added|removed
 """
 
 my_set = {"Jan","Feb","Mar","Apr"}
+for element in my_set:
+    print(element)
+print()
+    
+my_set.add("May")
+print(my_set)
+print()
