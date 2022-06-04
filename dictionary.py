19d30aef6be13606e2922563e4a83600b64204e8
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-04 13:59:26 +0200

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,20 +1,21 @@
 # https://www.youtube.com/watch?v=t8pPdKYpowI&t=4548s
 # 2:44:00
-# DICTIONARY DATA TYPE
 
-# The goal is to convert days to hours
-name_of_unit = "hours"
-conversion = 24
+"""DICTIONARY DATA TYPE
+- does not allow duplicate values
+- used to store values in |key:value| pairs
+"""
+
+# The goal is to convert days to another unit of time
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * conversion} {name_of_unit}"
+    return f"{num_of_days} days are {num_of_days * days_and_unit[0]} {days_and_unit[1]}"
 
 
 def validate_user_input():
     try: 
-        num_of_days = int(num_of_days_element)
-        # we want to convert only positive integers
-        if num_of_days > 0:  
+        num_of_days = int(days_and_unit[0])
+        if num_of_days > 0:                     # we want to convert only positive integers
             result = days_to_units(num_of_days)
             print(result)
         elif num_of_days == 0: 
@@ -22,14 +23,13 @@ def validate_user_input():
         else:
             print("You entered a negative value, which makes absolutely no sense.")    
     except ValueError:
-        print("Your input is not a valid number of days, don't try to break my program.")
+        print("Your input is not valid, don't try to break my program.")
 
 
-user_input = "" # variable initialization
+user_input = ""
 while user_input != "exit":
-    user_input = input("Hey user, enter a number of days and conversion unit:\n")
-    list_of_numbers = user_input.split(":") # we wait two inputs separated by a colon => number_of_days:conversion_unit
-    for num_of_days_element in user_input.split(","): # we expect a list of comma-separated values 
-        validate_user_input()
-    
+    user_input = input("Hey user, enter a number of days and a conversion unit:\n")
+    days_and_unit = user_input.split(":") # we wait two inputs separated by a colon => number_of_days:conversion_unit
+    print(days_and_unit)
+    {"days":20,"unit":10}
 # 2:47:00
