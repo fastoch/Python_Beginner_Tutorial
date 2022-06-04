1200efa6f13e300cd68271aae750276733ba7cdf
Fastoch <89261095+fastoch@users.noreply.github.com>
2022-06-04 09:15:33 +0200

    Add files via upload

---
+++ built-in_functions.py
@@ -0,0 +1,13 @@
+# https://www.youtube.com/watch?v=t8pPdKYpowI&t=4548s
+# 2:38:00
+
+# each data type has its own built-in functions
+
+print("some text")
+input("enter some value:")
+set([1,2,4,2]) # convert a list into a set
+int("2") # convert a string into an integer
+
+"2,3".split() # The split() method splits a string into a list
+
+# 2:42:00
